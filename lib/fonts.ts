ae1f32e09df9801965546ff4f896e96a51198660
Jeff Boutwell <jboutwell@us.ci.org>
2026-01-19 13:40:03 -0700

    add fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,13 @@
+import { Bitter, Inter, Loved_by_the_King } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+export const bitter = Bitter({
+  subsets: ["latin"],
+});
+
+export const lovedByTheKing = Loved_by_the_King({
+  weight: "400",
+  subsets: ["latin"],
+});
