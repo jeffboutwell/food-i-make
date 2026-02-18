8cae090e40972038a496aee63630d3f18fef1d0f
Jeff Boutwell <jboutwell@us.ci.org>
2026-02-18 08:19:42 -0700

    add authorId to each recipe

--- prisma/scripts/migrateIngredients.ts
+++ prisma/scripts/migrateIngredients.ts
@@ -16,26 +16,31 @@ async function migrateIngredients() {
   console.log("Starting ingredient migration...");
 
   console.log("Deleting all current rows...");
-  const deletedIngedients = await prisma.ingredient.deleteMany({});
-  const deletedSections = await prisma.ingredientSection.deleteMany({});
-  const deletedRecords = await prisma.recipe.deleteMany({});
+  await prisma.user.deleteMany({});
+  await prisma.ingredient.deleteMany({});
+  await prisma.ingredientSection.deleteMany({});
+  await prisma.recipe.deleteMany({});
   console.log("Deleted all current rows.");
 
-  /*   const recipes = await prisma.recipe.findMany({
-    include: {
-      sections: {
-        include: {
-          ingredients: true,
-        },
-      },
+  console.log("Creating default user jeffboutwell@gmail.com");
+  const defaultUser = await prisma.user.create({
+    data: {
+      authId: "115240539659829599294",
+      name: "Jeff Boutwell",
+      firstName: "Jeff",
+      lastName: "Boutwell",
+      email: "jeffboutwell@gmail.com",
     },
-  }); */
+  });
+
+  console.log("Default user created", defaultUser);
 
   console.log(`Found ${recipes.length} recipes to migrate.`);
 
   for (const recipe of recipes) {
     await prisma.recipe.create({
       data: {
+        authorId: defaultUser.id,
         ...recipe,
         sections: {
           create: recipe.sections.map((section, sectionIndex) => ({
