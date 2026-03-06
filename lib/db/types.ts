85d057e1704fe120b5e9c50ba8a5888fbc5ff58b
Jeff Boutwell <jboutwell@us.ci.org>
2026-03-06 13:36:45 -0700

    create PrismaJson types

---
+++ lib/db/types.ts
@@ -0,0 +1,15 @@
+// This file must be a module, so we include an empty export.
+export {};
+import { SourceProps } from "../schema";
+import { IngredientFormValues } from "./recipe/ingredient.schemas";
+import { IngredientSectionFormValues } from "./recipe/ingredient-section.schemas";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace PrismaJson {
+    // Define a type for a source of a recipe.
+    type Source = SourceProps;
+    type Ingredient = IngredientFormValues;
+    type IngredientSection = IngredientSectionFormValues;
+  }
+}
