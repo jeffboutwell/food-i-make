0c33971cf8222051536ab51e9a8d04111e495f09
Jeff Boutwell <jboutwell@us.ci.org>
2026-03-07 10:30:01 -0700

    update image types and schema

---
+++ lib/db/recipe/image.types.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+import { UploadResponse } from "@imagekit/next";
+
+export const ImageSchema = z.object({
+  image: z.object({
+    fileId: z.string(),
+    name: z.string(),
+    size: z.number(),
+    versionInfo: z.object({
+      id: z.string(),
+      name: z.string(),
+    }),
+    filePath: z.string(),
+    url: z.url(),
+    fileType: z.string(),
+    height: z.number(),
+    width: z.number(),
+    thumbnailUrl: z.url(),
+    AITags: z.array(z.string()).nullable(),
+    description: z.string().nullable(),
+  }),
+  alt: z.string().optional(),
+});
+
+export type Image = z.infer<typeof ImageSchema>;
+
+export type ImageResponse = UploadResponse;
