93fae224e801ccc94b8205014af6c968b4158009
Jeff Boutwell <jboutwell@us.ci.org>
2026-04-18 21:04:11 -0600

    add UserIcon atom

--- lib/components/atoms/user-icon/user-icon.tsx
+++ lib/components/atoms/user-icon/user-icon.tsx
@@ -2,73 +2,20 @@
 
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { User } from "lucide-react";
-import clsx from "clsx";
-
-const LoggedIn = () => {
-  return (
-    <DropdownMenuContent className="w-32" align="end">
-      <DropdownMenuGroup>
-        <DropdownMenuItem asChild>
-          <Link href="/profile">Profile</Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/recipe/create">Create Recipe</Link>
-        </DropdownMenuItem>
-      </DropdownMenuGroup>
-      <DropdownMenuSeparator />
-      <DropdownMenuGroup>
-        <DropdownMenuItem variant="destructive" onClick={() => signOut()}>
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuGroup>
-    </DropdownMenuContent>
-  );
-};
-
-const LoggedOut = () => {
-  return (
-    <DropdownMenuContent className="w-32" align="end">
-      <DropdownMenuGroup>
-        <DropdownMenuItem asChild>
-          <Link href="/profile">Log In</Link>
-        </DropdownMenuItem>
-      </DropdownMenuGroup>
-    </DropdownMenuContent>
-  );
-};
 
 export const UserIcon = ({ className }: { className?: string }) => {
   const { data: session } = useSession();
   return (
-    <div className={clsx("UserIcon", className)}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <Avatar>
-              <AvatarImage
-                src={session?.user?.image || ""}
-                referrerPolicy="no-referrer"
-              />
-              <AvatarFallback>
-                <User />
-              </AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        {session ? <LoggedIn /> : <LoggedOut />}
-      </DropdownMenu>
-    </div>
+    <Avatar className={className}>
+      <AvatarImage
+        src={session?.user?.image || ""}
+        referrerPolicy="no-referrer"
+      />
+      <AvatarFallback>
+        <User />
+      </AvatarFallback>
+    </Avatar>
   );
 };
