fde65f6adcbbcf331534b038917a2fc49bae6627
Jeff Boutwell <jboutwell@us.ci.org>
2026-05-06 17:00:38 -0600

    add user's recipes

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { auth } from "@/lib/auth";
 import { Profile } from "@/lib/components/organisms/profile/profile";
+import { getUserByEmail } from "@/lib/actions/user.actions";
 
 const Page = async () => {
   const session = await auth();
-
-  return <Profile session={session} />;
+  const user = await getUserByEmail(session?.user?.email ?? "");
+  return <Profile session={session} user={user} />;
 };
 
 export default Page;
--- lib/components/organisms/profile/profile.tsx
+++ lib/components/organisms/profile/profile.tsx
@@ -2,33 +2,48 @@ import React from "react";
 import Image from "next/image";
 import SignIn from "@/lib/components/molecules/auth/sign-in";
 import SignOut from "@/lib/components/molecules/auth/sign-out";
-import { H1 } from "@/lib/typography";
+import { H1, H2 } from "@/lib/typography";
 import { Session } from "next-auth";
+import { UserFull } from "@/lib/db/user";
+import { RecipeList } from "../recipe-list/recipe-list";
+import { RecipeCardVariant } from "../../molecules/recipe-card/recipe-card";
 
 type ProfileProps = {
   session: Session | null;
+  user: UserFull | null;
 };
 
-export const Profile = ({ session }: ProfileProps) => {
+export const Profile = ({ session, user }: ProfileProps) => {
   if (!session?.user) return <SignIn />;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="grid gap-24">
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div>
           <H1>{session.user.name ?? "Profile"}</H1>
-          <p>{session.user.email}</p>
+          <p className="mb-8">{session.user.email}</p>
+          <SignOut />
         </div>
         {session.user.image && (
-          <Image
-            src={session.user.image}
-            alt="User Avatar"
-            width={150}
-            height={150}
-          />
+          <div className="ml-auto">
+            <Image
+              src={session.user.image}
+              alt="User Avatar"
+              width={150}
+              height={150}
+            />
+          </div>
         )}
       </div>
-      <SignOut />
+      {user?.recipes && user.recipes.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <H2>My Recipes</H2>
+          <RecipeList
+            recipes={user?.recipes ?? []}
+            variant={RecipeCardVariant.COMPACT}
+          />
+        </div>
+      )}
     </div>
   );
 };
